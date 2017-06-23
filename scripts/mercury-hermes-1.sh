c7aae9fcb277b6bc1ddd38a6b66438ee7591a400
Adrian Brink <adrian@brink-holdings.com>
2017-06-23 17:48:40 +0200

    Add venus testnet

--- scripts/mercury-hermes-1.sh
+++ scripts/mercury-hermes-1.sh
@@ -9,9 +9,11 @@ export TMHOME_MERCURY=~/.cosmos-testnets/mercury/server/tendermint
 export BCHOME_HERMES_CLIENT=~/.cosmos-testnets/hermes/client
 export BCHOME_HERMES_SERVER=~/.cosmos-testnets/hermes/server
 export TMHOME_HERMES=~/.cosmos-testnets/hermes/server/tendermint
+export ETHHOME_VENUS_SERVER=~/.cosmos-testnets/venus/server
+export TMHOME_VENUS=~/.cosmos-testnets/venus/server/tendermint
 
 # make the directories
-mkdir -p $TMHOME_HERMES $TMHOME_MERCURY
+mkdir -p $TMHOME_HERMES $TMHOME_MERCURY $TMHOME_VENUS
 
 # create some aliases to use instead of raw `basecli` and `basecoin` to ensure we're using the right configuration for the chain we want to talk to.
 alias basecli-mercury="basecli --home $BCHOME_MERCURY_CLIENT"
@@ -20,10 +22,14 @@ alias basecoin-mercury="basecoin --home $BCHOME_MERCURY_SERVER"
 alias basecoin-hermes="basecoin --home $BCHOME_HERMES_SERVER"
 alias tendermint-mercury="tendermint --home $TMHOME_MERCURY"
 alias tendermint-hermes="tendermint --home $TMHOME_HERMES"
+alias tendermint-venus="tendermint --home $TMHOME_VENUS"
+alias ethermint-venus="ethermint --datadir $ETHHOME_VENUS_SERVER"
 
 # copy the genesis and config files
 cp mercury/tendermint/* $TMHOME_MERCURY
 cp mercury/basecoin/* $BCHOME_MERCURY_SERVER
 cp hermes/tendermint/* $TMHOME_HERMES
 cp hermes/basecoin/* $BCHOME_HERMES_SERVER
+cp venus/ethermint/* $ETHHOME_VENUS_SERVER
+cp venus/tendermint/* $TMHOME_VENUS
 
